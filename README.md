c8e59ff005ced30afea03e8e43db0a0ae605e7bc
DDimbe <109931951+DmytroDatsiuk@users.noreply.github.com>
2023-05-08 06:10:50 +0300

    all tasks hw 01

---
+++ README.md
@@ -0,0 +1,3 @@
+https://monosnap.com/file/Tkq5sdUaHNTq9FnClBPCu7IYObBEXy
+
+https://monosnap.com/file/bNm8Cx5GHJHTecVKxJDQOmeMuToWE7
---
+++ contacts.js
@@ -0,0 +1,35 @@
+const fs = require("fs/promises");
+const path = require("path");
+const crypto = require("crypto");
+
+const contactsPath = path.join(process.cwd(), "contacts", "contacts.json");
+
+async function listContacts() {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  return contacts.find((contact) => contact.id === contactId);
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const contactIdx = contacts.findIndex((contact) => contact.id === contactId);
+
+  contacts.splice(contactIdx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contactId;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: crypto.randomUUID(), name, email, phone };
+
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
